246bb65303679e426c5e590c7fa4f4f3e9c0f2a2
Ducthach <ducthachs3click@gmail.com>
2014-01-04 22:03:50 +0700

    Upgrade MT Review

---
+++ app/code/community/MT/Review/Block/Helper.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * ------------------------------------------------------------------------------
+ * @category     MT
+ * @package      MT_Review
+ * ------------------------------------------------------------------------------
+ * @copyright    Copyright (C) 2008-2013 MagentoThemes.net. All Rights Reserved.
+ * @license      GNU General Public License version 2 or later;
+ * @author       MagentoThemes.net
+ * @email        support@magentothemes.net
+ * ------------------------------------------------------------------------------
+ *
+ */
+class MT_Review_Block_Helper extends Mage_Review_Block_Helper
+{
+    public function getReviewsUrl()
+    {
+        return Mage::getUrl('mtreview/product/list', array(
+            'id'        => $this->getProduct()->getId(),
+            'category'  => $this->getProduct()->getCategoryId()
+        ));
+    }
+}
