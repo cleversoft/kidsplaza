e346bd02067872707d8e86b84a5dd36e7bfb98e4
nvlong <nvlong2203@gmail.com>
2014-01-09 14:12:35 +0700

    update css

---
+++ js/mt/extensions/jquery/compatibility.js
@@ -0,0 +1,22 @@
+/**
+ * @category    MT
+ * @package     KidsPlaza
+ * @copyright   Copyright (C) 2008-2013 MagentoThemes.net. All Rights Reserved.
+ * @license     GNU General Public License version 2 or later
+ * @author      MagentoThemes.net
+ * @email       support@magentothemes.net
+ */
+
+var isBootstrapEvent = false;
+if (window.jQuery) {
+    jQuery.noConflict();
+    jQuery('*').on('hide.bs.dropdown', function(){
+        isBootstrapEvent = true;
+    });
+    jQuery('*').on('hide.bs.collapse', function(){
+        isBootstrapEvent = true;
+    });
+    jQuery('*').on('hide.bs.modal', function(){
+        isBootstrapEvent = true;
+    });
+}
--- js/mt/kidsplaza/location.js
+++ js/mt/kidsplaza/location.js
@@ -1,7 +1,15 @@
+/**
+ * @category    MT
+ * @package     KidsPlaza
+ * @copyright   Copyright (C) 2008-2013 MagentoThemes.net. All Rights Reserved.
+ * @license     GNU General Public License version 2 or later
+ * @author      MagentoThemes.net
+ * @email       support@magentothemes.net
+ */
 'use strict';
 
-var Location = Class.create();
-Location.prototype = {
+var KidsPlazaLocation = Class.create();
+KidsPlazaLocation.prototype = {
     locationVar: 'location',
     initialize: function(){
         this.location = Mage.Cookies ? Mage.Cookies.get(this.locationVar) : null;
@@ -10,22 +18,15 @@ Location.prototype = {
         }
     },
     showLocationSelect: function(){
-        jQuery.fancybox({
-            type: 'html',
-            content: 'Choose location:<br><button type="button" class="button" onclick="window.loc.setLocation(\'hn\')"><span><span>Ha Noi</span></span></button>&nbsp;<button type="button" class="button" onclick="window.loc.setLocation(\'hcm\')"><span><span>Ho Chi Minh</span></span></button>',
-            modal: true
-        });
+
     },
     setLocation: function(location){
         var expire = new Date();
         expire.setDate(expire.getDate() + 30);
         Mage.Cookies.set(this.locationVar, location, expire);
         this.location = location;
-        jQuery.fancybox.close();
         window.location.reload();
     }
 };
 
-jQuery(function(){
-    window.loc = new Location();
-});
+window.KPLocation = new KidsPlazaLocation();
