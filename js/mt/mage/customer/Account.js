aa9b9ba5a64b5f84b48d07e38b4698918f23c977
nvlong <nvlong2203@gmail.com>
2014-01-13 16:41:05 +0700

    update css

---
+++ js/mt/mage/customer/Account.js
@@ -0,0 +1,11 @@
+/**
+ * @category    MT
+ * @package     KidsPlaza
+ * @copyright   Copyright (C) 2008-2013 MagentoThemes.net. All Rights Reserved.
+ * @license     GNU General Public License version 2 or later
+ * @author      MagentoThemes.net
+ * @email       support@magentothemes.net
+ */
+
+new VarienForm('login-form', true);
+new VarienForm('form-validate', true);
