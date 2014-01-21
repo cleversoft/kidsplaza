0297828dd46e3e2f3bc3522dbaf08b5dbdfdee9c
nvlong <nvlong22032gmail.com>
2014-01-21 16:48:02 +0700

    update productquestion

--- app/code/community/MT/ProductQuestions/sql/productquestions_setup/mysql4-install-2.2.0.php
+++ app/code/community/MT/ProductQuestions/sql/productquestions_setup/mysql4-install-2.2.0.php
@@ -13,26 +13,23 @@
  *
  */
 $installer = $this;
-
 $installer->startSetup();
-
 $installer->run("
-	-- DROP TABLE IF EXISTS {$this->getTable('productquestions/questions')};
-
-    CREATE TABLE {$this->getTable('productquestions/questions')} (
-     `question_id` int(10) unsigned NOT NULL auto_increment,
-     `question_status` tinyint(2) NOT NULL default '1',
-     `question_product_id` int(10) unsigned NOT NULL default '0',
-     `question_store_id` int(11) NOT NULL default '1' COMMENT 'asked from',
-     `question_store_ids` varchar(255) NOT NULL default '0' COMMENT 'displayed on',
-     `question_product_name` varchar(255) NOT NULL,
-     `question_author_name` varchar(255) NOT NULL,
-     `question_author_email` varchar(255) NOT NULL default '',
-     `question_date` datetime NOT NULL default '0000-00-00 00:00:00',
-     `question_text` text NOT NULL,
-     `question_reply_text` text NOT NULL,
-     PRIMARY KEY  (`question_id`),
-     KEY `question_status` (`question_status`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS {$this->getTable('productquestions/productquestions')};
+CREATE TABLE {$this->getTable('productquestions/productquestions')} (
+    `question_id` int(10) unsigned NOT NULL auto_increment,
+    `question_status` tinyint(2) NOT NULL default '1',
+    `question_product_id` int(10) unsigned NULL default null,
+    `question_store_id` int(11) NOT NULL COMMENT 'asked from',
+    `question_store_ids` varchar(255) NOT NULL COMMENT 'displayed on',
+    `question_product_name` varchar(255) NULL DEFAULT NULL,
+    `question_author_name` varchar(255) NOT NULL,
+    `question_author_email` varchar(255) NOT NULL,
+    `question_date` datetime NOT NULL,
+    `question_text` text NOT NULL,
+    `question_reply_text` text NOT NULL,
+    PRIMARY KEY (`question_id`),
+    KEY `question_status` (`question_status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ");
 $installer->endSetup();
