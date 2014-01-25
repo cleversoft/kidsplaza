31b51b9ed086c1426903563c0df4ccbd960e5d04
ducthach <ducthachs3click@gmail.com>
2014-01-25 08:56:57 +0700

    Update product questions

--- app/code/community/MT/ProductQuestions/sql/productquestions_setup/mysql4-install-2.2.0.php
+++ app/code/community/MT/ProductQuestions/sql/productquestions_setup/mysql4-install-2.2.0.php
@@ -18,7 +18,6 @@ $installer->run("
 --DROP TABLE IF EXISTS {$this->getTable('productquestions/productquestions')};
 CREATE TABLE {$this->getTable('productquestions/productquestions')} (
     `question_id` int(10) unsigned NOT NULL auto_increment,
-    `parent_question_id` int(10) unsigned NOT NULL default '0',
     `question_status` tinyint(2) NOT NULL default '1',
     `question_product_id` int(10) unsigned NULL default null,
     `question_store_id` int(11) NOT NULL COMMENT 'asked from',
@@ -29,7 +28,6 @@ CREATE TABLE {$this->getTable('productquestions/productquestions')} (
     `question_date` datetime NOT NULL,
     `question_text` text NOT NULL,
     `question_reply_text` text NOT NULL,
-    `hits` int(10) unsigned NOT NULL default '0',
     PRIMARY KEY (`question_id`),
     KEY `question_status` (`question_status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
