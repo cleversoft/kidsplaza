c7956d12dadd5811963e3d4eea895230b9cb34ab
nvlong <nvlong22032gmail.com>
2014-03-25 09:46:51 +0700

    fix #11

---
+++ app/code/local/MT/KidsPlaza/controllers/Wishlist/IndexController.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * @category    MT
+ * @package     MT_KidsPlaza
+ * @copyright   Copyright (C) 2008-2013 MagentoThemes.net. All Rights Reserved.
+ * @license     GNU General Public License version 2 or later
+ * @author      MagentoThemes.net
+ * @email       support@magentothemes.net
+ */
+require_once 'Mage/Wishlist/controllers/IndexController.php';
+class MT_KidsPlaza_Wishlist_IndexController extends Mage_Wishlist_IndexController{
+    /**
+     * Update wishlist item comments
+     */
+    public function updateAction()
+    {
+        if (!$this->_validateFormKey()) {
+            return $this->_redirect('*/*/');
+        }
+        $wishlist = $this->_getWishlist();
+        if (!$wishlist) {
+            return $this->norouteAction();
+        }
+
+        $post = $this->getRequest()->getPost();
+        if ($post && isset($post['description']) && is_array($post['description'])) {
+            $updatedItems = 0;
+
+            foreach ($post['description'] as $itemId => $description) {
+                $item = Mage::getModel('wishlist/item')->load($itemId);
+                if ($item->getWishlistId() != $wishlist->getId()) {
+                    continue;
+                }
+
+                // Extract new values
+                $description = (string)$description;
+
+                if ($description == Mage::helper('wishlist')->defaultCommentString()) {
+                    $description = '';
+                } elseif (!strlen($description)) {
+                    $description = $item->getDescription();
+                }
+
+                $qty = null;
+                if (isset($post['qty'][$itemId])) {
+                    $qty = $this->_processLocalizedQty($post['qty'][$itemId]);
+                }
+                if (is_null($qty)) {
+                    $qty = $item->getQty();
+                    if (!$qty) {
+                        $qty = 1;
+                    }
+                } elseif (0 == $qty) {
+                    try {
+                        $item->delete();
+                    } catch (Exception $e) {
+                        Mage::logException($e);
+                        Mage::getSingleton('customer/session')->addError(
+                            $this->__('Can\'t delete item from wishlist')
+                        );
+                    }
+                }
+
+                // Check that we need to save
+                if (($item->getDescription() == $description) && ($item->getQty() == $qty)) {
+                    continue;
+                }
+                try {
+                    $item->setDescription($description)
+                        ->setQty($qty)
+                        ->save();
+                    $updatedItems++;
+                } catch (Exception $e) {
+                    Mage::getSingleton('customer/session')->addError(
+                        $this->__('Can\'t save description %s', Mage::helper('core')->escapeHtml($description))
+                    );
+                }
+            }
+
+            // save wishlist model for setting date of last update
+            if ($updatedItems) {
+                try {
+                    $wishlist->save();
+                    Mage::helper('wishlist')->calculate();
+                    Mage::getSingleton('customer/session')->addSuccess($this->__('Wishlist updated.'));
+                } catch (Exception $e) {
+                    Mage::getSingleton('customer/session')->addError($this->__('Can\'t update wishlist'));
+                }
+            }
+
+            if (isset($post['save_and_share'])) {
+                $this->_redirect('*/*/share', array('wishlist_id' => $wishlist->getId()));
+                return;
+            }
+        }
+        $this->_redirect('*', array('wishlist_id' => $wishlist->getId()));
+    }
+
+    /**
+     * Remove item
+     */
+    public function removeAction()
+    {
+        $id = (int) $this->getRequest()->getParam('item');
+        $item = Mage::getModel('wishlist/item')->load($id);
+        if (!$item->getId()) {
+            return $this->norouteAction();
+        }
+        $wishlist = $this->_getWishlist($item->getWishlistId());
+        if (!$wishlist) {
+            return $this->norouteAction();
+        }
+        try {
+            $item->delete();
+            $wishlist->save();
+            Mage::getSingleton('customer/session')->addSuccess(
+                $this->__('Wishlist updated.')
+            );
+        } catch (Mage_Core_Exception $e) {
+            Mage::getSingleton('customer/session')->addError(
+                $this->__('An error occurred while deleting the item from wishlist: %s', $e->getMessage())
+            );
+        } catch (Exception $e) {
+            Mage::getSingleton('customer/session')->addError(
+                $this->__('An error occurred while deleting the item from wishlist.')
+            );
+        }
+
+        Mage::helper('wishlist')->calculate();
+
+        $this->_redirectReferer(Mage::getUrl('*/*'));
+    }
+}
