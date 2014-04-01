8cdb5e28010d018c0772c50841cffe4e2d821b44
nvlong <nvlong22032gmail.com>
2014-04-01 15:04:36 +0700

    update price all

--- js/mt/mage/catalog/ProductView.js
+++ js/mt/mage/catalog/ProductView.js
@@ -14,6 +14,176 @@ var RelatedPrice = Class.create(Product.OptionsPrice, {
     }
 });
 
+var ProductOptionPrice = Class.create(Product.OptionsPrice, {
+    initPrices: function() {
+        this.containers[0] = 'product-price-' + this.productId;
+        this.containers[1] = 'bundle-price-' + this.productId;
+        this.containers[2] = 'price-including-tax-' + this.productId;
+        this.containers[3] = 'price-excluding-tax-' + this.productId;
+        this.containers[4] = 'old-price-' + this.productId;
+        Event.observe('qty', 'change', function(event){
+            var elm = event.findElement('input'),
+                qty = elm.value;
+            qty = qty <= 0 ? 1 : (qty > 99 ? 99 : qty);
+            elm.value = qty;
+            this.reload();
+        }.bind(this));
+    },
+
+    reload: function() {
+        var price;
+        var formattedPrice;
+        var optionPrices = this.getOptionPrices();
+        var nonTaxable = optionPrices[1];
+        var optionOldPrice = optionPrices[2];
+        var priceInclTax = optionPrices[3];
+        optionPrices = optionPrices[0];
+
+        $H(this.containers).each(function(pair) {
+            var _productPrice;
+            var _plusDisposition;
+            var _minusDisposition;
+            var _priceInclTax;
+            if ($(pair.value)) {
+                if (pair.value == 'old-price-'+this.productId && this.productOldPrice != this.productPrice) {
+                    _productPrice = this.productOldPrice;
+                    _plusDisposition = this.oldPlusDisposition;
+                    _minusDisposition = this.oldMinusDisposition;
+                } else {
+                    _productPrice = this.productPrice;
+                    _plusDisposition = this.plusDisposition;
+                    _minusDisposition = this.minusDisposition;
+                }
+                _priceInclTax = priceInclTax;
+
+                if (pair.value == 'old-price-'+this.productId && optionOldPrice !== undefined) {
+                    price = optionOldPrice+parseFloat(_productPrice);
+                } else if (this.specialTaxPrice == 'true' && this.priceInclTax !== undefined && this.priceExclTax !== undefined) {
+                    price = optionPrices+parseFloat(this.priceExclTax);
+                    _priceInclTax += this.priceInclTax;
+                } else {
+                    price = optionPrices+parseFloat(_productPrice);
+                    _priceInclTax += parseFloat(_productPrice) * (100 + this.currentTax) / 100;
+                }
+
+                if (this.specialTaxPrice == 'true') {
+                    var excl = price;
+                    var incl = _priceInclTax;
+                } else if (this.includeTax == 'true') {
+                    // tax = tax included into product price by admin
+                    var tax = price / (100 + this.defaultTax) * this.defaultTax;
+                    var excl = price - tax;
+                    var incl = excl*(1+(this.currentTax/100));
+                } else {
+                    var tax = price * (this.currentTax / 100);
+                    var excl = price;
+                    var incl = excl + tax;
+                }
+
+                var subPrice = 0;
+                var subPriceincludeTax = 0;
+                Object.values(this.customPrices).each(function(el){
+                    if (el.excludeTax && el.includeTax) {
+                        subPrice += parseFloat(el.excludeTax);
+                        subPriceincludeTax += parseFloat(el.includeTax);
+                    } else {
+                        subPrice += parseFloat(el.price);
+                        subPriceincludeTax += parseFloat(el.price);
+                    }
+                });
+                excl += subPrice;
+                incl += subPriceincludeTax;
+
+                if (typeof this.exclDisposition == 'undefined') {
+                    excl += parseFloat(_plusDisposition);
+                }
+
+                incl += parseFloat(_plusDisposition) + parseFloat(this.plusDispositionTax);
+                excl -= parseFloat(_minusDisposition);
+                incl -= parseFloat(_minusDisposition);
+
+                //adding nontaxlable part of options
+                excl += parseFloat(nonTaxable);
+                incl += parseFloat(nonTaxable);
+
+                if (pair.value == 'price-including-tax-'+this.productId) {
+                    price = incl;
+                } else if (pair.value == 'price-excluding-tax-'+this.productId) {
+                    price = excl;
+                } else if (pair.value == 'old-price-'+this.productId) {
+                    if (this.showIncludeTax || this.showBothPrices) {
+                        price = incl;
+                    } else {
+                        price = excl;
+                    }
+                } else {
+                    if (this.showIncludeTax) {
+                        price = incl;
+                    } else {
+                        price = excl;
+                    }
+                }
+
+                if (price < 0) price = 0;
+
+                if (price > 0 || this.displayZeroPrice) {
+                    formattedPrice = this.formatPrice(price);
+                } else {
+                    formattedPrice = '';
+                }
+
+                if ($(pair.value).select('.price')[0]) {
+                    $(pair.value).select('.price')[0].innerHTML = formattedPrice;
+                    if ($(pair.value+this.duplicateIdSuffix) && $(pair.value+this.duplicateIdSuffix).select('.price')[0]) {
+                        $(pair.value+this.duplicateIdSuffix).select('.price')[0].innerHTML = formattedPrice;
+                    }
+                } else {
+                    $(pair.value).innerHTML = formattedPrice;
+                    if ($(pair.value+this.duplicateIdSuffix)) {
+                        $(pair.value+this.duplicateIdSuffix).innerHTML = formattedPrice;
+                    }
+                }
+            };
+        }.bind(this));
+
+        for (var i = 0; i < this.tierPrices.length; i++) {
+            $$('.price.tier-' + i).each(function (el) {
+                var price = this.tierPrices[i] + parseFloat(optionPrices);
+                el.innerHTML = this.formatPrice(price);
+            }, this);
+            $$('.price.tier-' + i + '-incl-tax').each(function (el) {
+                var price = this.tierPricesInclTax[i] + parseFloat(optionPrices);
+                el.innerHTML = this.formatPrice(price);
+            }, this);
+            $$('.benefit').each(function (el) {
+                var parsePrice = function (html) {
+                    return parseFloat(/\d+\.?\d*/.exec(html));
+                };
+                var container = $(this.containers[3]) ? this.containers[3] : this.containers[0];
+                var price = parsePrice($(container).innerHTML);
+                var tierPrice = $$('.tier-price.tier-' + i+' .price');
+                tierPrice = tierPrice.length ? parsePrice(tierPrice[0].innerHTML, 10) : 0;
+                var $percent = Selector.findChildElements(el, ['.percent.tier-' + i]);
+                $percent.each(function (el) {
+                    el.innerHTML = Math.ceil(100 - ((100 / price) * tierPrice));
+                });
+            }, this);
+        }
+
+        if ($('product-price-total')){
+            var container = $('product-price-total'),
+                qty = $('qty').value;
+            if (isNaN(qty)) qty = 1;
+            var total = price * qty;
+            if (container.select('.price')[0]){
+                container.select('.price')[0].innerHTML = this.formatPrice(total);
+            }else{
+                container.innerHTML = this.formatPrice(total);
+            }
+        }
+    }
+});
+
 document.on('dom:loaded', function(){
     if (window.spConfigDataEnhanced){
         window.spConfig = new Product.EnhancedConfig(window.spConfigDataEnhanced);
@@ -26,11 +196,12 @@ jQuery('.product-view').each(function(i, product){
     var id = jQuery(product).data('product');
     if (!id) return;
 
-    if (window['price' + id]) window.optionsPrice = new Product.OptionsPrice(window['price' + id]);
+    if (window['price' + id]) window.optionsPrice = new ProductOptionPrice(window['price' + id]);
+    window.optionsPrice.reload();
     if (window['related' + id]){
-        window['related'+id].productPrice = 0;
-        window['related'+id].priceExclTax = 0;
-        window['related'+id].priceInclTax = 0;
+        window['related' + id].productPrice = 0;
+        window['related' + id].priceExclTax = 0;
+        window['related' + id].priceInclTax = 0;
         window.relatedPrice = new RelatedPrice(window['related' + id]);
         addRelatedToProduct();
         window.relatedProductsCheckFlag = false;
