09d968956103f68de11fa980b0d6b36570769267
nvlong <nvlong22032gmail.com>
2014-04-08 09:13:29 +0700

    fix checkout

---
+++ app/code/local/MT/KidsPlaza/controllers/Checkout/CartController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @category    MT
+ * @package     MT_KidsPlaza
+ * @copyright   Copyright (C) 2008-2013 MagentoThemes.net. All Rights Reserved.
+ * @license     GNU General Public License version 2 or later
+ * @author      MagentoThemes.net
+ * @email       support@magentothemes.net
+ */
+require_once 'Mage/Checkout/controllers/CartController.php';
+class MT_KidsPlaza_Checkout_CartController extends Mage_Checkout_CartController{
+    /**
+     * Set back redirect url to response
+     *
+     * @return Mage_Checkout_CartController
+     * @throws Mage_Exception
+     */
+    protected function _goBack()
+    {
+        $returnUrl = $this->getRequest()->getParam('return_url');
+        if ($returnUrl) {
+
+            if (!$this->_isUrlInternal($returnUrl)) {
+                throw new Mage_Exception('External urls redirect to "' . $returnUrl . '" denied!');
+            }
+
+            $this->_getSession()->getMessages(true);
+            $this->getResponse()->setRedirect($returnUrl);
+        } elseif (!Mage::getStoreConfig('checkout/cart/redirect_to_cart')
+            && !$this->getRequest()->getParam('in_cart')
+            && $backUrl = $this->_getRefererUrl()
+        ) {
+            $this->getResponse()->setRedirect($backUrl);
+        } else {
+            if (($this->getRequest()->getActionName() == 'add') && !$this->getRequest()->getParam('in_cart')) {
+                $this->_getSession()->setContinueShoppingUrl($this->_getRefererUrl());
+            }
+            $this->_redirect('checkout/onepage');
+        }
+        return $this;
+    }
+}
