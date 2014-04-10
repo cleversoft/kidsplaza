63ca9b873a9801aaa5689bab0fe02295793f4eef
nvlong <nvlong22032gmail.com>
2014-04-10 15:26:43 +0700

    fix search

--- js/mt/kidsplaza/location.js
+++ js/mt/kidsplaza/location.js
@@ -122,9 +122,9 @@ var KidsPlazaSearch = Class.create(Varien.searchForm, {
             updateElement: this._selectAutocompleteItem.bind(this),
             onShow: function(element, update){
                 update.style.width = element.getWidth() + 7 + 'px';
-                //update.style.height = 'auto';
+                update.style.height = 'auto';
                 update.style.opacity = 1;
-                Effect.SlideDown(update, {duration: 0.2});
+                Effect.Appear(update, {duration: 0.2});
             }
         });
     },
