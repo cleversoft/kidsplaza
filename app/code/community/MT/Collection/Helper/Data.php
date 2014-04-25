66260ef83a3608cf9d993e23b00e88d406601b4a
nvlong <nvlong2203@gmail.com>
2014-04-25 16:58:53 +0700

    update translate

---
+++ app/code/community/MT/Collection/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @category    MT
+ * @package     MT_Collection
+ * @copyright   Copyright (C) 2008-2013 MagentoThemes.net. All Rights Reserved.
+ * @license     GNU General Public License version 2 or later
+ * @author      MagentoThemes.net
+ * @email       support@magentothemes.net
+ */
+class MT_Collection_Helper_Data extends Mage_Core_Helper_Abstract{
+
+}
