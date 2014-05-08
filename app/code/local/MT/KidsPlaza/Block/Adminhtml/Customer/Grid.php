9bbbde373733f867211976959cafffee9b0cf474
nvlong <nvlong22032gmail.com>
2014-05-08 15:05:28 +0700

    update admin customer grid

---
+++ app/code/local/MT/KidsPlaza/Block/Adminhtml/Customer/Grid.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * @category    MT
+ * @package     MT_KidsPlaza
+ * @copyright   Copyright (C) 2008-2013 MagentoThemes.net. All Rights Reserved.
+ * @license     GNU General Public License version 2 or later
+ * @author      MagentoThemes.net
+ * @email       support@magentothemes.net
+ */
+class MT_KidsPlaza_Block_Adminhtml_Customer_Grid extends Mage_Adminhtml_Block_Customer_Grid{
+    protected function _prepareCollection()
+    {
+        $collection = Mage::getResourceModel('customer/customer_collection')
+            ->addNameToSelect()
+            ->addAttributeToSelect('email')
+            ->addAttributeToSelect('created_at')
+            ->addAttributeToSelect('group_id')
+            ->addAttributeToSelect('phone_number')
+            ->joinAttribute('billing_postcode', 'customer_address/postcode', 'default_billing', null, 'left')
+            ->joinAttribute('billing_city', 'customer_address/city', 'default_billing', null, 'left')
+            ->joinAttribute('billing_telephone', 'customer_address/telephone', 'default_billing', null, 'left')
+            ->joinAttribute('billing_region', 'customer_address/region', 'default_billing', null, 'left')
+            ->joinAttribute('billing_country_id', 'customer_address/country_id', 'default_billing', null, 'left');
+
+        $this->setCollection($collection);
+
+        return Mage_Adminhtml_Block_Widget_Grid::_prepareCollection();
+    }
+
+    protected function _prepareColumns()
+    {
+        $this->addColumn('entity_id', array(
+            'header'    => Mage::helper('customer')->__('ID'),
+            'width'     => '50px',
+            'index'     => 'entity_id',
+            'type'  => 'number',
+        ));
+        /*$this->addColumn('firstname', array(
+            'header'    => Mage::helper('customer')->__('First Name'),
+            'index'     => 'firstname'
+        ));
+        $this->addColumn('lastname', array(
+            'header'    => Mage::helper('customer')->__('Last Name'),
+            'index'     => 'lastname'
+        ));*/
+        $this->addColumn('name', array(
+            'header'    => Mage::helper('customer')->__('Name'),
+            'index'     => 'name'
+        ));
+        $this->addColumn('email', array(
+            'header'    => Mage::helper('customer')->__('Email'),
+            'width'     => '150',
+            'index'     => 'email'
+        ));
+
+        $groups = Mage::getResourceModel('customer/group_collection')
+            ->addFieldToFilter('customer_group_id', array('gt'=> 0))
+            ->load()
+            ->toOptionHash();
+
+        $this->addColumn('group', array(
+            'header'    =>  Mage::helper('customer')->__('Group'),
+            'width'     =>  '100',
+            'index'     =>  'group_id',
+            'type'      =>  'options',
+            'options'   =>  $groups,
+        ));
+
+        $this->addColumn('Telephone', array(
+            'header'    => Mage::helper('customer')->__('Telephone'),
+            'width'     => '100',
+            'index'     => 'phone_number'
+        ));
+
+        $this->addColumn('customer_since', array(
+            'header'    => Mage::helper('customer')->__('Customer Since'),
+            'type'      => 'datetime',
+            'align'     => 'center',
+            'index'     => 'created_at',
+            'gmtoffset' => true
+        ));
+
+        if (!Mage::app()->isSingleStoreMode()) {
+            $this->addColumn('website_id', array(
+                'header'    => Mage::helper('customer')->__('Website'),
+                'align'     => 'center',
+                'width'     => '80px',
+                'type'      => 'options',
+                'options'   => Mage::getSingleton('adminhtml/system_store')->getWebsiteOptionHash(true),
+                'index'     => 'website_id',
+            ));
+        }
+
+        $this->addColumn('action',
+            array(
+                'header'    =>  Mage::helper('customer')->__('Action'),
+                'width'     => '100',
+                'type'      => 'action',
+                'getter'    => 'getId',
+                'actions'   => array(
+                    array(
+                        'caption'   => Mage::helper('customer')->__('Edit'),
+                        'url'       => array('base'=> '*/*/edit'),
+                        'field'     => 'id'
+                    )
+                ),
+                'filter'    => false,
+                'sortable'  => false,
+                'index'     => 'stores',
+                'is_system' => true,
+            ));
+
+        $this->addExportType('*/*/exportCsv', Mage::helper('customer')->__('CSV'));
+        $this->addExportType('*/*/exportXml', Mage::helper('customer')->__('Excel XML'));
+        return Mage_Adminhtml_Block_Widget_Grid::_prepareColumns();
+    }
+}
